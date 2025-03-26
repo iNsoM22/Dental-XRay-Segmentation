0c987aea124c67a9314b5eb59fa4148b75a3ccd3
asta <s.a.moizkhan988@gmail.com>
2025-03-26 19:44:29 +0500

    Included Single Routes Export

---
+++ dentai/src/routes/pages.tsx
@@ -0,0 +1,9 @@
+import About from "./about/AboutPage";
+import Contact from "./contact/ContactPage";
+import Home from "./home/HomePage";
+import PredictionPage from "./predict/PredictionPage";
+
+export const HomePage = <Home />;
+export const PredictionsPage = <PredictionPage />;
+export const ContactsPage = <Contact />;
+export const AboutPage = <About />;
